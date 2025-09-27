cf71d0fd725c444805aee561d43f3178df572f0e
Oleg Karasik <karasik.oleg.nikolaevich@gmail.com>
2025-09-27 12:27:13 +0300

    Implemented support for season folders renaming

--- src/Commands/NormaliseCommand.cs
+++ src/Commands/NormaliseCommand.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using MediaLibrary.Business;
 using MediaLibrary.Business.Items;
 using MediaLibrary.Extensions.Services;
@@ -30,8 +31,17 @@ public partial class NormaliseCommand : AsyncCommand<NormaliseCommandSettings>
   }
 
   private static void RenameItem(
+    Item<DirectoryPath> item,
+    string name)
+  {
+    ArgumentNullException.ThrowIfNull(item);
+    ArgumentException.ThrowIfNullOrEmpty(name);
+
+    Directory.Move(item.Path.Value, item.Path.WithName(name).Value);
+  }
+
+  private static void RenameItem(
     Item<FilePath> item,
-    string directory,
     string name)
   {
     ArgumentNullException.ThrowIfNull(item);
@@ -42,8 +52,181 @@ public partial class NormaliseCommand : AsyncCommand<NormaliseCommandSettings>
       File.Move(
         file,
         Path.Combine(
-          directory,
+          item.Path.Directory,
           Path.GetFileName(file).Replace(item.Path.Name, name)));
+    }
+  }
+
+  private static void MoveItem(
+    Item<DirectoryPath> item,
+    string directory)
+  {
+    ArgumentNullException.ThrowIfNull(item);
+    ArgumentException.ThrowIfNullOrEmpty(directory);
+
+    Directory.Move(item.Path.Value, directory);
+  }
+
+  private static void MoveItem(
+    Item<FilePath> item,
+    string directory)
+  {
+    ArgumentNullException.ThrowIfNull(item);
+    ArgumentException.ThrowIfNullOrEmpty(directory);
+
+    foreach (var file in Directory.EnumerateFiles(item.Path.Directory, $"*{item.Path.Name}.*"))
+    {
+      File.Move(
+        file,
+        Path.Combine(
+          directory,
+          Path.GetFileName(file)));
+    }
+  }
+
+  private EpisodeItem ProcessEpisode(
+    ShowItem show,
+    SeasonItem season,
+    EpisodeItem episode)
+  {
+    ArgumentNullException.ThrowIfNull(show);
+    ArgumentNullException.ThrowIfNull(season);
+    ArgumentNullException.ThrowIfNull(episode);
+
+    string name;
+    if (episode.Position.HasSpan)
+    {
+      var (Open, Close) = episode.Position.GetSpanPosition();
+      name = $"S{episode.Position.GetGroup():D2}E{Open:D2}-E{Close:D2} - {episode.Title}";
+    }
+    else
+    {
+      name = $"S{episode.Position.GetGroup():D2}E{episode.Position.GetPosition():D2} - {episode.Title}";
+    }
+
+    if (!episode.Path.Name.Equals(name))
+    {
+      this.statistics.WriteUpdated();
+
+      AnsiConsole.MarkupLineInterpolated(
+        $"[[[Yellow]U[/]]]: Renamed {episode.Path.Name} [Yellow]->[/] {name}");
+
+      RenameItem(episode, name);
+
+      return new EpisodeItem
+      {
+        Title = episode.Title,
+        Position = episode.Position,
+        Path = new FilePath(Path.Combine(episode.Path.Directory, $"{name}{episode.Path.Extension}"))
+      };
+    }
+
+    return episode;
+  }
+
+  private SeasonItem ProcessSeason(
+    ShowItem show,
+    SeasonItem season)
+  {
+    ArgumentNullException.ThrowIfNull(show);
+    ArgumentNullException.ThrowIfNull(season);
+
+    string name;
+    if (season.Position.GetPosition() == 0)
+    {
+      name = $"Specials";
+    }
+    else
+    {
+      name = $"Season {season.Position.GetPosition()}";
+    }
+
+    if (!season.Path.Name.Equals(name))
+    {
+      var directory = Path.Combine(show.Path.Value, name);
+      if (season.Path.Value.Equals(show.Path.Value))
+      {
+        this.statistics.WriteUpdated();
+
+        AnsiConsole.MarkupLineInterpolated(
+          $"[[[Yellow]U[/]]]: Grouped (Season) {season.Path.Name} [Yellow]->[/] {name}");
+
+        // All seasons are located in the "show" directory, therefore 
+        // we need to create dedicated show directories and move all episodes
+        // there
+        //
+        Directory.CreateDirectory(directory);
+
+        foreach (var episode in season.Episodes)
+        {
+          MoveItem(episode, directory);
+        }
+      }
+      else
+      {
+        this.statistics.WriteUpdated();
+
+        AnsiConsole.MarkupLineInterpolated(
+          $"[[[Yellow]U[/]]]: Renamed (Season) {season.Path.Name} [Yellow]->[/] {name}");
+
+        // We need to rename season directory
+        //
+        RenameItem(season, name);
+      }
+
+      // Here, we update the "season" variable to ensure
+      // we can proceed with the processing of the episodes
+      //
+      season = new SeasonItem
+      {
+        Title = name,
+        Position = season.Position,
+        Path = new DirectoryPath(directory),
+        Episodes = [..
+          season.Episodes.Select(episode => new EpisodeItem
+          {
+            Title = episode.Title,
+            Position = episode.Position,
+            Path = new FilePath(Path.Combine(directory, $"{episode.Path.Name}{episode.Path.Extension}"))
+          })
+        ]
+      };
+    }
+    return new SeasonItem
+    {
+      Title = season.Title,
+      Position = season.Position,
+      Path = season.Path,
+      Episodes = [..Process(show, season)]
+    };
+
+    IEnumerable<EpisodeItem> Process(ShowItem show, SeasonItem season)
+    {
+      foreach (var episode in season.Episodes)
+      {
+        yield return this.ProcessEpisode(show, season, episode);
+      }
+    }
+  }
+
+  private ShowItem ProcessShow(
+    ShowItem show)
+  {
+    ArgumentNullException.ThrowIfNull(show);
+    
+    return new ShowItem
+    {
+      Title = show.Title,
+      Path = show.Path,
+      Seasons = [.. Process(show)]
+    };
+
+    IEnumerable<SeasonItem> Process(ShowItem show)
+    {
+      foreach (var season in show.Seasons)
+      {
+        yield return this.ProcessSeason(show, season);
+      }
     }
   }
 
@@ -63,78 +246,35 @@ public partial class NormaliseCommand : AsyncCommand<NormaliseCommandSettings>
               ctx.Status("Normalising...");
 
               var index = await FileServices.LoadAsync(new FilePathIndex(settings.Library));
-
-              foreach (var show in index.Shows)
+              index = new IndexItem
               {
-                foreach (var season in show.Seasons)
-                {
-                  // Here we need to understand, whether the show has "seasonal" 
-                  // structure (i.e. every season is located within a unique, correctly named directory
-                  // and all episodes are in these "seasonal" directories.
-                  //
-                  var normaliseDirectory = false;
+                Path = index.Path,
+                Movies = index.Movies,
+                Shows = [.. Process(index.Shows)]
+              };
 
-                  var directory = season.Path.Value;
-                  if (!season.Path.Name.Equals(season.Title))
-                  {
-                    directory = Path.Combine(show.Path.Value, season.Title);
-
-                    // Here, we create a directory were all episodes
-                    // would be moved
-                    //
-                    Directory.CreateDirectory(directory);
-
-                    normaliseDirectory = true;
-                  }
-
-                  foreach (var episode in season.Episodes)
-                  {
-                    string name;
-                    if (episode.Position.HasSpan)
-                    {
-                      var (Open, Close) = episode.Position.GetSpanPosition();
-                      name = $"S{episode.Position.GetGroup():D2}E{Open:D2}-E{Close:D2} - {episode.Title}";
-                    }
-                    else
-                    {
-                      name = $"S{episode.Position.GetGroup():D2}E{episode.Position.GetPosition():D2} - {episode.Title}";
-                    }
-
-                    var normaliseName = !episode.Path.Name.Equals(name);
-                    
-                    if (normaliseName || normaliseDirectory)
-                    {
-                      this.statistics.WriteUpdated();
-
-                      if (normaliseName)
-                      {
-                        AnsiConsole.MarkupLineInterpolated(
-                          $"[[[Yellow]U[/]]]: {episode.Path.Name} [Yellow]->[/] {name}");
-                      }
-                      if (normaliseDirectory)
-                      {
-                        AnsiConsole.MarkupLineInterpolated(
-                          $"[[[Yellow]U[/]]]: {episode.Path.Name} [Yellow]->[/] {directory}");
-                      }
-
-                      RenameItem(episode, directory, name);
-                    }
-                  }
-                }
-              }
+              await FileServices.SaveAsync(index, index.Path);
             });
 
           if (this.statistics.HasUpdates)
           {
             AnsiConsole.Write(new Rule());
             AnsiConsole.MarkupLineInterpolated($"[[[Green]S[/]]]: Updated - [Underline]{this.statistics.Updated}[/], Elapsed - [Underline]{measurement.Elapsed}[/].");
-            AnsiConsole.MarkupLineInterpolated($"[[[Red]W[/]]]: The index is [Underline]out of date[/].");
+            AnsiConsole.MarkupLineInterpolated($"[[[Green]S[/]]]: The index is [Underline]updated[/].");
           }
           else
           {
             AnsiConsole.MarkupLineInterpolated($"[[[Green]S[/]]]: Updated - [Underline]None[/], Elapsed - [Underline]{measurement.Elapsed}[/].");
           }
         });
+
+    IEnumerable<ShowItem> Process(IEnumerable<ShowItem> shows)
+    {
+      foreach (var show in shows)
+      {
+        yield return this.ProcessShow(show);
+      }
+    }
 
     return 0;
   }
